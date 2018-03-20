7f071b954691ac188ad0bbdebdfad1e7df63f768
Anthony Veaudry <anthony@veaudry.pro>
2018-03-20 14:07:02 +0200

    import js into jade file for less server requests
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

--- docs/js/script.js
+++
@@ -1,1 +0,0 @@
-document.addEventListener("DOMContentLoaded",function(d){d.target.body.classList.add("loaded")});
--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,2 @@
-// adds a loaded class to the body when the body is fully loaded
-document.addEventListener("DOMContentLoaded", function(event){
-  event.target.body.classList.add("loaded");
-});
+  //- adds a loaded class to the body when the body is fully loaded
+  document.addEventListener("DOMContentLoaded", function(event){event.target.body.classList.add("loaded");});
