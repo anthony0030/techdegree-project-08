3e1210c0208752ba8845373e5cd36fb94f95a841
Anthony Veaudry <anthony@veaudry.pro>
2022-11-09 18:19:59 +0200

    Repairs project
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

--- source/js/script.js
+++ source/js/script.js
@@ -1,2 +1,2 @@
-  //- adds a loaded class to the body when the body is fully loaded
+  //? adds a loaded class to the body when the body is fully loaded
   document.addEventListener("DOMContentLoaded", function(event){event.target.body.classList.add("loaded");});
